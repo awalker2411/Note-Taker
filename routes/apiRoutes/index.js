29c20c4a64232ca0d6683be046d9076e3674e27d
Anthony Walker <awalker2411@yahoo.com>
2023-03-12 16:18:00 -0400

    added route for notes with help of classmate

--- routes/apiRoutes/index.js
+++
@@ -1,4 +0,0 @@
-const app = require(`express`).Router();
-const uniqid = require(`uniqid`);
-const fs = require(`fs`);
-const { readAndAppend, readFromFile, writeToFile } = require(`../../helpers/utils`)
